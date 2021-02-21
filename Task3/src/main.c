05e7b906ef4b0716d68b7d076d8e91afc14dad83
DESKTOP-UOTH3TJ\Eugene <johnynymous@gmail.com>
2021-02-21 23:19:25 +0200

    Added implementation for getting dir. info in 2 threads.

--- Task3/src/main.c
+++ Task3/src/main.c
@@ -1,44 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <pthread.h>
-#include <windows.h>
+#include <pthread.h>
 #include <dirent.h>
 #include <string.h>
 
-void print_elems_full_path(char *f_name);
-void print_folder_elems(char *f_name);
-//void get_folder_info(char *f_name, int (*operation)(char *));
+void* print_elems_full_path(char *f_name);
+void* print_folder_elems(char *f_name);
 void get_full_path(char *, char *, char **);
 
 enum {COUNT_ELEMS, GET_FULL_PATH};
 
 int main(int argc, char *argv[]) {
   if(argc < 2) {
-    fprintf(stderr, "Folder path's does not specify.\n");
+    fprintf(stderr, "Folder path does not specify.\n");
     exit(0);
   }
 
   char *f_name = *++argv;
-  //pthread_t thread1, thread2;
+  int rc1, rc2;
+  pthread_t thread1, thread2;
   
-
-  if((d = opendir(f_name)) != NULL) {
-    int elems = 0;
-    while((dir = readdir(d)) != NULL) {
-      char *dir_name = dir->d_name;
-      if(!strcmp(dir_name, "."))
-	continue;
-      if(!strcmp(dir_name, ".."))
-	continue;
-      char *fp;
-      get_full_path(f_name, dir_name, &fp);
-      printf("%s\n", fp);
-      elems++;
-      free(fp);
-    }
-    printf("\nElements in folder: %d", elems);
-    closedir(d);
+  if((rc1 = pthread_create(&thread1, NULL, print_folder_elems, (void*)f_name))) {
+    fprintf(stderr, "Thread creation failed: %d.\n", rc1);
+    exit(0);
   }
+  
+  if((rc2 = pthread_create(&thread1, NULL, print_folder_elems, (void*)f_name))) {
+    fprintf(stderr, "Thread creation failed: %d.\n", rc2);
+    exit(0);
+  }
+
+  thread_join(thread1, NULL);
+  thread_join(thread2, NULL);
+  
   return 0;
 }
 
@@ -49,28 +43,27 @@ void get_full_path(char *f_name, char *dir_name, char **fullPath) {
   strcat(*fullPath, dir_name);
 }
 
-/* void get_folder_info(char *f_name, int (*operation)(char *)) { */
-
-/* } */
-
-void print_folder_elems(char *f_name) {
+void* print_folder_elems(void *threadArg) {
+  char *f_name = (char *)threadArg;
   DIR *d;
   struct dirent *dir;
   if((d = opendir(f_name)) != NULL) {
-  int elems = 0;
-  while((dir = readdir(d)) != NULL) {
-    char *dir_name = dir->d_name;
-    if(!strcmp(dir_name, "."))
-      continue;
-    if(!strcmp(dir_name, ".."))
-      continue;
-    elems++;
+    int elems = 0;
+    while((dir = readdir(d)) != NULL) {
+      char *dir_name = dir->d_name;
+      if(!strcmp(dir_name, "."))
+	continue;
+      if(!strcmp(dir_name, ".."))
+	continue;
+      elems++;
+    }
   }
   printf("Folder contains %d elements.\n", elems);
   closedir(d);  
 }
 
-void print_elems_full_path(char *f_name) {
+void* print_elems_full_path(void *threadArg) {
+  char *f_name = (char *)threadArg;
   DIR *d;
   struct dirent *dir;
   if((d = opendir(f_name)) != NULL) {
